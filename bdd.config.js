245a02d5922602927e3f1866636264fa8f615943
MykhailKo <tuareg1812@gmail.com>
2022-08-31 12:23:46 +0300

    init

---
+++ bdd.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+	"featuresPath": "./bdd/features",
+	"stepsPath": "./dist/bdd/steps",
+	"contextsPath": "./dist/bdd/contexts",
+	"bootstrapPath": "./bdd/DBBootstrap.config.js",
+	"db": {
+		"user": "",
+		"passwd": "",
+		"host": "",
+		"port": 0,
+		"database": ""
+	},
+	"envsPath": "",
+	"threadsNumber": 1,
+	"reporters": [
+		"default"
+	],
+	"webapp": {
+		"host": "",
+		"port": 0
+	}
+}
